52486a8a514679b3a200656e659332b83e76b42f
wolanx <825407762@qq.com>
2023-04-03 20:01:53 +0800

    ignore

--- src/main/java/jii/db/BeanHelper.java
+++ src/main/java/jii/db/BeanHelper.java
@@ -1,12 +1,12 @@
 package jii.db;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-
 import java.beans.PropertyDescriptor;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class BeanHelper {
 
@@ -21,8 +21,9 @@ public class BeanHelper {
         Set<String> emptyNames = new HashSet<>();
         for (PropertyDescriptor pd : pds) {
             Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null)
+            if (srcValue == null) {
                 emptyNames.add(pd.getName());
+            }
         }
         return emptyNames.toArray(new String[0]);
     }
